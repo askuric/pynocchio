930b022f759d523d97509ff8131be3083a6e4ded
askuric <you@example.com>
2023-02-28 12:45:36 +0100

    v1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pynocchio',
-    version='1.0',
+    version='1.1',
     description='A pip package implementing a simple wrapper of the pinocchio robot model.',
     url='https://gitlab.inria.fr/auctus-team/people/antunskuric/pynocchio',
     author='Antun Skuric',

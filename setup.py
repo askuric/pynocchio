6ce1c430aacc81ff1eede81d310b424440ba454d
askuric <you@example.com>
2023-02-28 10:54:09 +0100

    typo

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='pypinocchio',
+setup(name='pynocchio',
     version='0.1',
     description='A pip package implementing a simple wrapper of the pinocchio robot model.',
     url='https://gitlab.inria.fr/auctus-team/people/antunskuric/pynocchio',
     author='Antun Skuric',
     author_email='antun.skuric@inria.fr',
     license='MIT',
-    packages=['pypinocchio'],
-    install_requires=['numpy'],
+    packages=['pynocchio'],
+    install_requires=['pinocchio','numpy'],
     zip_safe=False)
